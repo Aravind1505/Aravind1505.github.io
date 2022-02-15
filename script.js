09471e308ae6463df097fc0818252c9704f14c3a
Aravind1505 <karavind246815@gmail.com>
2022-02-16 02:07:58 +0530

    ListUpdated
    Content List Now displayed its corresponding Items.
    
    Note: Project and Courses need to be updated

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-document.getElementById("content-info-skills").appendChild(document.createTextNode("Water"));
+const skills_list_array = ["Java", "C/C++", "Python", "React", "HTML", "JavaScript", "CSS", "OpenGL", "p5.js", "Bootstrap", "SASS", "Redux"];
+const skills_list_array_html = [];
+
+for (let i = 0; i < skills_list_array.length; i++) {
+    skills_list_array_html.push(document.createElement("li"));
+}
+
+for (let i = 0; i < skills_list_array_html.length; i++) {
+    skills_list_array_html[i].id = "Skill" + (i+1);
+    skills_list_array_html[i].innerHTML = skills_list_array[i];
+    document.getElementById("content-info-skills-list").appendChild(skills_list_array_html[i]);
+}
+
+// document.getElementById("content-info-skills-list").appendChild(document.createElement("li"));
