cd1d03a1163c1b074230c3307fed6bb44263a12f
Aravind1505 <karavind246815@gmail.com>
2022-02-17 21:47:54 +0530

    Complete
    Website Complete with only HTML CSS JavaScript.

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ for (let i = 0; i < skills_list_array_html.length; i++) {
 }
 
 // document.getElementById("content-info-skills-list").appendChild(document.createElement("li"));
+//Courses
+
+const courses_list_array = ["FrontEnd - FreeCodeCamp", "CyberSecurity - EC", "PythonSpecilization - Coursera", "HTML/JavaScript - FreeCodeCamp"];
+const courses_list_array_html = [];
+
+for (let i = 0; i < courses_list_array.length; i++) {
+    courses_list_array_html.push(document.createElement("li"));
+}
+
+for (let i = 0; i < courses_list_array_html.length; i++) {
+    courses_list_array_html[i].id = "Course" + (i+1);
+    courses_list_array_html[i].innerHTML = courses_list_array[i];
+    document.getElementById("content-info-courses-list").appendChild(courses_list_array_html[i]);
+}
+
+//Projects
+
+const projects_list_array = ["Inventory Management", "Java Basic Utility Toolkit", "Resume Web", "Physics Engine Web p5js"];
+const projects_list_array_html = [];
+
+for (let i = 0; i < projects_list_array.length; i++) {
+    projects_list_array_html.push(document.createElement("li"));
+}
+
+for (let i = 0; i < projects_list_array_html.length; i++) {
+    projects_list_array_html[i].id = "Project" + (i+1);
+    projects_list_array_html[i].innerHTML = projects_list_array[i];
+    document.getElementById("content-info-projects-list").appendChild(projects_list_array_html[i]);
+}
